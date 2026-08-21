3a85e9918c9e53a123ec98afd4513c9e709bfbee
ChawSu-coder <136878636+ChawSu-coder@users.noreply.github.com>
2026-08-22 01:41:33 +0630

    Attack Success Rate v3

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -101,6 +101,11 @@
                                                      document.getElementById('logout-form').submit();">
                                             {{ __('Logout') }}
                                         </a>
+                                        <a class="dropdown-item" href="{{ route('logout') }}"
+                                           onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                            {{ __('Remove from all devices') }}
+                                        </a>
 
                                         <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                                             @csrf
